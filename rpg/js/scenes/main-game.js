940c8c625ce7b66c8b7fdee2a3bd9099b4d527c7
Jon <lylyahiko@gmail.com>
2019-07-26 13:59:31 -0400

    Added couple more things and added comments

--- rpg/js/scenes/main-game.js
+++ rpg/js/scenes/main-game.js
@@ -1,13 +1,51 @@
+/*
+    This scene controls all the functionality for the main game
+ */
 class MainGame extends Phaser.Scene {
+    // Our constructor, set the scene key
     constructor() {
         super({key:"MainGame"});
     }
 
+    // Preload our assets we use for this scene
     preload() {
-
+        // Preload the player (ship)
+        this.load.spritesheet('ship',
+            'assets/ship.png',
+            { frameWidth: 16, frameHeight: 16 }
+        );
+        // Preload the expolosion for dead player (ship)
+        this.load.spritesheet('explosion',
+            'assets/explosion.png',
+            { frameWidth: 16, frameHeight: 16 }
+        );
     }
 
+    // Create all the assets for this scene as well as add functionality
     create() {
-        this.text = this.add.text(0,0, "Build some gameplay!!", {font:"40px Impact"});
+        // Create the player, place in dead center
+        this.player = this.add.sprite(config.width / 2, config.height / 2, 'ship');
+        // Double the size of the sprite since it's super tiny
+        this.player.setScale(2);
+
+        // Create the animation for the ship idling
+        this.anims.create({
+            key: "idle",
+            frames: this.anims.generateFrameNumbers('ship'),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        // Play the idle animation
+        this.player.play('idle');
+
+        // Create the animation for the ship exploding
+        this.anims.create({
+            key: "explode",
+            frames: this.anims.generateFrameNumbers('explosion'),
+            frameRate: 20,
+            repeat: 0,
+            hideOnComplete: true
+        });
     }
 }
--- rpg/js/scenes/title.js
+++ rpg/js/scenes/title.js
@@ -1,19 +1,29 @@
+/*
+    This scene controls all the functionality for the title
+ */
 class Title extends Phaser.Scene {
+    // Our constructor, set the scene key
     constructor() {
         super({key:"Title"});
     }
 
+    // Preload our assets we use for this scene
     preload() {
         this.load.image('sky', 'assets/sky.png');
         this.load.image('star', 'assets/star.png');
     }
 
+    // Create all the assets for this scene as well as add functionality
     create() {
-        this.sky = this.add.image(400, 300, 'sky');
+        // Our sky is our background
+        this.sky = this.add.image(0, 0, 'sky');
+        this.sky.setOrigin(0, 0);
 
+        // Add welcome and continue text
         this.welcome = this.add.text(0,0, "Welcome to some RPG Game!", {font:"40px Impact"});
-        this.continue = this.add.text(200,300, "Press Enter To Continue", {font:"40px Impact"});
+        this.continue = this.add.text(150,300, "Press Enter or Click Me To Continue", {font:"40px Impact"});
 
+        // This controls the weird movement of the welcome text
         this.tweens.add({
             targets: this.welcome,
             x:200,
@@ -24,14 +34,24 @@ class Title extends Phaser.Scene {
             delay: 1000
         }, this);
 
+        // If we press the enter key then start the game (main-game scene)
         this.input.keyboard.on('keyup', function(event) {
             if (event.key === "Enter") {
                 this.scene.start("MainGame");
             }
         }, this);
+
+        // Set the continue text to be interactive
+        this.continue.setInteractive();
+        // If we click the text start the main-game scene
+        this.continue.on('pointerdown', () => {
+            this.scene.start("MainGame");
+        })
     }
 
+    // Check for updates
     update() {
+        // If we click anywhere that's not the text create stars cause why not
         if (game.input.mousePointer.isDown) {
             let physicsImage = this.physics.add.image(game.input.mousePointer.x, game.input.mousePointer.y, "star");
             physicsImage.setVelocity(Phaser.Math.RND.integerInRange(-200, 200), -300);
